586c405cb164d40fb2f2235b7998a86a7af416ad
iammayank <34920570+mayank-aggrwal@users.noreply.github.com>
2022-10-01 21:02:03 +0530

    Update FakePersonDataAccessService.java

--- src/main/java/com/may/rest/dao/FakePersonDataAccessService.java
+++ src/main/java/com/may/rest/dao/FakePersonDataAccessService.java
@@ -19,42 +19,4 @@ public class FakePersonDataAccessService implements PersonDao {
     private static List<Person> DB = new ArrayList<>();
     private static final Logger LOGGER = LogManager.getLogger(FakePersonDataAccessService.class);
 
-    @Override
-    public int insertPerson(UUID id, Person person) {
-        LOGGER.info("Inserting into DB, person with id: {}, name: {}", id, person.getName());
-        DB.add(new Person(id, person.getName()));
-        return 1;
-    }
-
-    @Override
-    public List<Person> selectAllPeople() {
-        LOGGER.info("Getting all people.");
-        return DB;
-    }
-
-    @Override
-    public int deletePerson(UUID id) {
-        Optional<Person> personMayBe = selectPersonById(id);
-        if(personMayBe.isEmpty()) return 0;
-        DB.remove(personMayBe.get());
-        return 1;
-    }
-
-    @Override
-    public int updatePerson(UUID id, Person person) {
-        return selectPersonById(id)
-                .map(p -> {
-                    int index = DB.indexOf(p);
-                    if(index >= 0) {
-                        DB.set(index, new Person(p.getId(), person.getName()));
-                        return 1;
-                    }
-                    return 0;
-                }).orElse(0);
-    }
-
-    @Override
-    public Optional<Person> selectPersonById(UUID id) {
-        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
-    }
 }
